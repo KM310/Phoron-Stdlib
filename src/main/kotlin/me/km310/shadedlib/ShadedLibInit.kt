3ec29e3c552d5da93da92537f4b7bc2bfbf2044c
K_M <julius@marcel-loos.de>
2026-06-15 20:28:12 +0200

    Add ShadedLibInit to initialize the library
    Implement ShadedLibInit as a ModInitializer for the library.

---
+++ src/main/kotlin/me/km310/shadedlib/ShadedLibInit.kt
@@ -0,0 +1,9 @@
+package me.km310.shadedlib
+
+import net.fabricmc.api.ModInitializer
+
+object ShadedLibInit : ModInitializer {
+    override fun onInitialize() {
+        // absichtlicher no-op: nur damit die Lib als Mod existiert
+    }
+}
